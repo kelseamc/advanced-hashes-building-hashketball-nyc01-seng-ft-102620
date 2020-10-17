a60135c17e704b176dfbe6e00b79f6b68ddbbed0
Kelsea McAllister <kelsea.mcall@gmail.com>
2020-10-17 15:15:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,24 @@
-# Write your code here!
+def game_hash
+  hash = {
+    home: {
+      team_name: "",
+      colors: [],
+      players: [
+        {:player_name 
+          :number
+          :shoe
+          :points
+          :rebounds
+          :assists
+          :steals
+          :blocks
+          :slam_dunks},
+      ]
+    },
+    away: {
+      team_name: "",
+      colors: [],
+      players: []
+    }
+  }
+end
