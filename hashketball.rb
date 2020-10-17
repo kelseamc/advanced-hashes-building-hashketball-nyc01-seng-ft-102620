f1454875e94d496fb685d79d56ae78b75e209785
Kelsea McAllister <kelsea.mcall@gmail.com>
2020-10-17 15:34:33 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -90,7 +90,7 @@ def game_hash
           assists: 2,
           steals: 1,
           blocks: 1,
-          slam_dunks: 0},  
+          slam_dunks: 0},
         {player_name: "Kemba Walker",
           number: 33,
           shoe: 15,
